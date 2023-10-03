a27a08a9deb02acece30f42eeae76cb1e2cb4a89
BahruDa <bahrudari68@gmail.com>
2023-10-03 08:07:43 +0300

    hot fix

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,33 @@
-const express = require('express');
-const AppController = require('../controllers/AppController');
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-function controllerRouting(app) {
-  const router = express.Router();
-  app.use(express.json());
-  app.use('/', router);
+const router = Router();
 
-  router.get('/status', (req, res) => {
-    AppController.getStatus(req, res);
-  });
+router.get('/status', AppController.getStatus);
 
-  router.get('/stats', (req, res) => {
-    AppController.getStats(req, res);
-  });
-}
+router.get('/stats', AppController.getStats);
 
-module.exports = controllerRouting;
+router.post('/users', UsersController.postNew);
+
+router.get('/connect', AuthController.getConnect);
+
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
+
+module.exports = router;
