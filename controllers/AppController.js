71196fe4d25fba37b63de0a277ade96e95630396
BahruDa <bahrudari68@gmail.com>
2023-10-03 08:13:45 +0300

    hot fix

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,15 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(request, response) {
-    return response.status(200).send(
-      `{"redis": ${redisClient.isAlive()}, "db": ${dbClient.isAlive()}}`,
-    );
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
   static async getStats(request, response) {
-    const nbusers = await dbClient.nbUsers();
-    const nbfiles = await dbClient.nbFiles();
-    return response.status(200).send(
-      `{"users": ${nbusers}, "files": ${nbfiles}}`,
-    );
+    const usersNum = await dbClient.nbUsers();
+    const filesNum = await dbClient.nbFiles();
+    response.status(200).json({ users: usersNum, files: filesNum });
   }
 }
 
